5e108fed52952e7ab4c8e2d818b3e8e8e6a3b178
sagar2751 <sagarmurthy2751@outlook.com>
2024-06-07 10:43:23 +0530

    Maruti page

---
+++ AutomationInstagram/src/cars/MarutiClass.java
@@ -0,0 +1,13 @@
+package cars;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class MarutiClass 
+{
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+		System.out.println("Maruti page executed");
+	}
+
+}
